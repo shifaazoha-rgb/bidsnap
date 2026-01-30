6f3fd87103201fbbaa7ede3d3a4a49bba4d47b9c
shifaazoha-rgb <shifaa.zoha@gmail.com>
2026-01-30 19:41:35 +0530

    BidSnap frontend

--- push-to-github.bat
+++ push-to-github.bat
@@ -1,14 +1,20 @@
 @echo off
-cd /d "%~dp0"
+cd /d "C:\Users\PC\Documents\bidssnap"
 
-if not exist .git git init
-git remote get-url origin 2>nul || git remote add origin https://github.com/shifaazoha-rgb/bidsnap.git
+if not exist .git (
+    git init
+    git remote add origin https://github.com/shifaazoha-rgb/bidsnap.git
+) else (
+    git remote get-url origin 2>nul
+    if errorlevel 1 git remote add origin https://github.com/shifaazoha-rgb/bidsnap.git
+)
 
 git add .
 git status
-git commit -m "BidSnap frontend: landing, estimate form, quote dashboard"
+git commit -m "BidSnap frontend"
 git branch -M main
 git push -u origin main
 
+echo.
 echo Done! View at https://github.com/shifaazoha-rgb/bidsnap
 pause
